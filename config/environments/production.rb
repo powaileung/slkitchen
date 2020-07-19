40a60ff77d26f66e184d6967166196fa382d6f75
Powai Leung <pwleung1489@gmail.com>
2020-07-19 21:11:31 +0000

    Input gem files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'slkitchen-powai-leung.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
